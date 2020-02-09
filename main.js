baaf5605e3c03f6b177f239b68db1bb79fe00e6d
MonsieurBibo <biboselim1308@gmail.com>
2020-02-09 18:49:05 +0100

    rework

---
+++ main.js
@@ -0,0 +1,11 @@
+/*el = document.getElementById('mid');
+if(el){
+    el.addEventListener("mouseover", function(){
+        el.style.flexGrow = 3;
+    });
+}
+
+
+/*document.getElementById('mid').addEventListener("mouseout", function(){
+    document.getElementById('mid').style.flexGrow = 1;
+});*/
